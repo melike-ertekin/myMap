81a8e183c6f8769ad200a5d969d6937bc3d5f754
mertekin3 <melike.ertekin@gatech.edu>
2019-02-27 15:26:40 -0500

    Draw vertical lines

--- app/src/main/java/com/example/android/mymap/MapsActivity.java
+++ app/src/main/java/com/example/android/mymap/MapsActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -35,6 +36,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
 
     private GoogleMap mMap;
     private UiSettings mUiSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,7 +106,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
         Log.d("onCameraIdle","The camera has stopped moving.");
        // Toast.makeText(this, "The camera has stopped moving.", Toast.LENGTH_SHORT).show();
 
-        calculateCorners();
+        DrawLines();
     }
 
 
@@ -113,25 +115,66 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnCamera
 
 
 
-    private void calculateCorners() {
+    private void DrawLines() {
 
         Projection projection = mMap.getProjection();
 
         VisibleRegion visibleRegion = projection.getVisibleRegion();
 
-        LatLng corner = projection.fromScreenLocation(new Point(0, 0));
-        Log.d("Projection","North East : "+visibleRegion.latLngBounds.northeast.latitude + ","+visibleRegion.latLngBounds.northeast.longitude);
-        Log.d("Projection","South West : "+visibleRegion.latLngBounds.southwest.latitude + ","+visibleRegion.latLngBounds.southwest.longitude);
+
+        //LatLng northEast = new LatLng(visibleRegion.latLngBounds.northeast.latitude,visibleRegion.latLngBounds.northeast.longitude);
+        //LatLng southWest = new LatLng(visibleRegion.latLngBounds.southwest.latitude,visibleRegion.latLngBounds.southwest.longitude);
+        //Log.d("Projection","North East : "+visibleRegion.latLngBounds.northeast.latitude + ","+visibleRegion.latLngBounds.northeast.longitude);
+        //Log.d("Projection","South West : "+visibleRegion.latLngBounds.southwest.latitude + ","+visibleRegion.latLngBounds.southwest.longitude);
+
+        Point corner = new Point(0, 0);
+        Point northEast = projection.toScreenLocation(visibleRegion.latLngBounds.northeast);
+        Point southWest = projection.toScreenLocation(visibleRegion.latLngBounds.southwest);
+
+
+        double width = Math.sqrt(Math.pow(northEast.x, 2) + Math.pow(northEast.y,2));
+        double hight = Math.sqrt(Math.pow(southWest.x, 2) + Math.pow(southWest.y,2));
+
+        int oneWidthUnit = (int)(width/4);
+        int hight2 = (int) hight;
+
+        LatLng hps1 = projection.fromScreenLocation(new Point(oneWidthUnit, 0));
+        LatLng hps2 = projection.fromScreenLocation(new Point(oneWidthUnit*2, 0));
+        LatLng hps3 = projection.fromScreenLocation(new Point(oneWidthUnit*3, 0));
+        //LatLng hps4 = projection.fromScreenLocation(new Point(oneWidthUnit*4, 0));
+
+        LatLng hpe1 = projection.fromScreenLocation(new Point(oneWidthUnit, hight2));
+        LatLng hpe2 = projection.fromScreenLocation(new Point(oneWidthUnit*2, hight2));
+        LatLng hpe3 = projection.fromScreenLocation(new Point(oneWidthUnit*3, hight2));
+
+        PolylineOptions randomLine1 = new PolylineOptions()
+                .add(new LatLng(hps1.latitude, hps1.longitude))
+                .add(new LatLng(hpe1.latitude, hpe1.longitude));
+        Polyline polyline1 = mMap.addPolyline(randomLine1);
+
+        PolylineOptions randomLine2 = new PolylineOptions()
+                .add(new LatLng(hps2.latitude, hps2.longitude))
+                .add(new LatLng(hpe2.latitude, hpe2.longitude));
+        Polyline polyline2 = mMap.addPolyline(randomLine2);
+
+        PolylineOptions randomLine3 = new PolylineOptions()
+                .add(new LatLng(hps3.latitude, hps3.longitude))
+                .add(new LatLng(hpe3.latitude, hpe3.longitude));
+        Polyline polyline3 = mMap.addPolyline(randomLine3);
 
 
 
+
+/*
         PolylineOptions randomLine1 = new PolylineOptions()
                 .add(new LatLng(visibleRegion.latLngBounds.northeast.latitude, visibleRegion.latLngBounds.northeast.longitude))
                 .add(new LatLng(visibleRegion.latLngBounds.southwest.latitude, visibleRegion.latLngBounds.southwest.longitude)) ;
         Polyline polyline1 = mMap.addPolyline(randomLine1);
-
+*/
 
     }
+
+
 
     private void settingsForMap() {
         mUiSettings = mMap.getUiSettings();
